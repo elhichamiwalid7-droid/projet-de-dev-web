1ae9d52df448e1d814a05930ea5ab408cb0501c1
Walid <elhichamiwalid7@gmail.com>
2026-04-30 17:26:26 +0100

    j'ai termine les pages 1,2,3 et 4

--- main.js
+++ main.js
@@ -31,3 +31,45 @@ window.onscroll = function(){
     }
     
 }
+
+
+// قائمة الأنميات (يمكنك تعديل الأسماء والتصنيفات هنا)
+const animes = [
+    { title: "Attack on Titan", genre: "Action,bloody,imagination", image: "./Projet_web/attack.png" },
+    { title: "Death Note", genre: "excitability, Ambiguity", image: "./Projet_web/death_note.jpg" },
+    { title: "Hunter x Hunter", genre: "imagination,adventures", image: "./Projet_web/Hunter.jpg" },
+    { title: "One Piece", genre: "Action,adventures,Comedy", image: "./Projet_web/one_piece.png" },
+    { title: "Demon Slayer", genre: "Action,demons,historic", image: "./Projet_web/demon.png" },
+    { title: "Jujutsu Kaisen", genre: "Action,supernatural", image: "./Projet_web/jujutsu.png" },
+    { title: "Naruto Shippuden", genre: "Action,Ninja", image: "./Projet_web/Narotu.png" },
+    { title: "Vinland Saga", genre: "Viking,philosophical", image: "./Projet_web/Vinland.png" },
+    { title: "Fullmetal Alchemist", genre: "Action,magic,drama", image: "./Projet_web/fullmetal.png" },
+    { title: "blue lock", genre:"Athlete,enthusiastic", image: "./Projet_web/blue_lock.png" }
+];
+
+// دالة لتوليد البطاقات وإضافتها إلى الصفحة
+function displayAnimes() {
+    const container = document.getElementById('anime-container');
+
+    animes.forEach(anime => {
+        // إنشاء عنصر البطاقة
+        const card = document.createElement('div');
+        card.className = 'anime-card';
+
+        // وضع هيكل البطاقة (صورة + عنوان + تصنيف)
+        // إذا كان مسار الصورة فارغاً, ستظهر مساحة رمادية فارغة
+        card.innerHTML = `
+            <img src="${attack.png}" alt="${attack_on_titan}" class="anime-image">
+            <div class="anime-info">
+                <h3>${anime.title}</h3>
+                <span class="genre">${anime.genre}</span>
+            </div>
+        `;
+
+        // إضافة البطاقة إلى الحاوية الرئيسية
+        container.appendChild(card);
+    });
+}
+
+// تشغيل الدالة عند تحميل الصفحة
+window.onload = displayAnimes;
